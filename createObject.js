7c5e52405554921260b70261dab6778a1efb2990
Dimas Wihandono <daimessdn@gmail.com>
2021-03-04 18:53:04 +0700

    add dynamic timing feature and added more mountain objects

--- createObject.js
+++ createObject.js
@@ -24,6 +24,8 @@ function createTree(x, y) {
 
 	treeObject.name = "tree" + generateTime();
 
+	treeObject.castShadow = true;
+
 	return treeObject;
 }
 
@@ -33,7 +35,7 @@ function createHumanObject() {
 
 	// create pointer
 	const pointerGeometry = new THREE.PlaneGeometry(1, 2);
-	const pointerMaterial = new THREE.MeshBasicMaterial();
+	const pointerMaterial = new THREE.MeshPhongMaterial();
 
 	const pointerMesh = new THREE.Mesh(pointerGeometry, pointerMaterial);
 	pointerMesh.position.y = 3;
@@ -41,7 +43,7 @@ function createHumanObject() {
 
 	// create box geometry for body
 	const geometry = new THREE.BoxGeometry(2, 2, 4);
-	const greenMaterial = new THREE.MeshPhongMaterial({ emissive: 0x00ff00 });
+	const greenMaterial = new THREE.MeshPhongMaterial({ emissive: 0xf5c77f });
 
 	// circle body for head
 	const circle = new THREE.SphereGeometry(1);
@@ -55,26 +57,54 @@ function createHumanObject() {
 	humanObject.add(headMesh);
 
 	humanObject.name = "human" + generateTime();
+	humanObject.castShadow = true;
 
 	return humanObject;
 }
 
 const createMountain = (x, y) => {
 	const mountain1Geometry = new THREE.ConeGeometry( 20, 20, 8 );
-	const mountain1Material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	const mountain1Material = new THREE.MeshPhongMaterial( {emissive: 0xa3cfe9} );
 	const mountain = new THREE.Mesh(mountain1Geometry, mountain1Material);
-	mountain.position.set(33, 33, 0);
+	mountain.position.set(x, y, 5);
 	mountain.rotation.x = Math.PI / 2;
+	mountain.castShadow = true;
 
-	mouuntain.name = "mountain" + generateTime();
+	mountain.name = "mountain" + generateTime();
 
 	return mountain;
+}
+
+const createMountain2 = (x, y) => {
+	const mountain2Geometry = new THREE.ConeGeometry( 20, 40, 20 );
+	const mountain2Material = new THREE.MeshPhongMaterial( {emissive: 0x013a89} );
+	const mountain2 = new THREE.Mesh(mountain2Geometry, mountain2Material);
+	mountain2.position.set(x, y, 18);
+	mountain2.rotation.x = Math.PI / 2;
+	mountain2.castShadow = true;
+
+	mountain2.name = "mountain" + generateTime();
+
+	return mountain2;
+}
+
+const createMountain3 = (x, y) => {
+	const mountain3Geometry = new THREE.ConeGeometry( 10, 30, 15 );
+	const mountain3Material = new THREE.MeshPhongMaterial( {emissive: 0x6098d1} );
+	const mountain3 = new THREE.Mesh(mountain3Geometry, mountain3Material);
+	mountain3.position.set(x, y, 13);
+	mountain3.rotation.x = Math.PI / 2;
+	mountain3.castShadow = true;
+
+	mountain3.name = "mountain3" + generateTime();
+
+	return mountain3;
 }
 
 const createLand = function() {
 	// create land surface
 	const landGeometry = new THREE.BoxGeometry(200, 200, 5);
-	const landMaterial = new THREE.MeshPhongMaterial( { emissive: 0x004d00 } );
+	const landMaterial = new THREE.MeshPhongMaterial( { emissive: 0x7cfc00 } );
 
 	const landMesh = new THREE.Mesh(landGeometry, landMaterial);
 	landMesh.position.z = -5;
@@ -85,4 +115,5 @@ const createLand = function() {
 }
 
 
-export default { createTree, createHumanObject, createLand, createMountain };
+export default { createTree, createHumanObject, createLand,
+	             createMountain, createMountain2, createMountain3 };
--- main.js
+++ main.js
@@ -21,9 +21,19 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
-// const raycaster = new THREE.Raycaster();
-
-// console.log(performance.now())
+const getTimeScenery = (time) => {
+	if (time >= 6 && time < 12) {
+    	scene.background = new THREE.Color(0x2e4482);
+			spotLight.position.set(105, 105, 10);
+	} else if (time >= 12 && time < 18) {
+    	scene.background = new THREE.Color(0x546bab);
+			spotLight.position.set(0, 0, 100);
+			spotLight.intensity = .25;
+	} else {
+    	scene.background = new THREE.Color(0x2e4482);
+			spotLight.position.set(0, 0, 0);
+  }
+}
 
 // init'd LocalStorage key
 const OBJECT_LOCAL_KEY = "park3d.objects";
@@ -33,25 +43,36 @@ if (!localStorage.getItem(OBJECT_LOCAL_KEY)) {
 	localStorage.setItem(OBJECT_LOCAL_KEY, "[]")
 }
 
-const objects = JSON.parse(localStorage.getItem(OBJECT_LOCAL_KEY));
-
 // create scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const land = createObject.createLand();
 scene.add(land);
-objects.push(land);
+
+const human = createObject.createHumanObject();
+scene.add(human);
+
+let objects = JSON.parse(localStorage.getItem(OBJECT_LOCAL_KEY));
+
+var axes = new THREE.AxesHelper(20);
+scene.add(axes);
 
 camera.position.z = 200;
 camera.position.y = -5;
 
 objects.forEach((obj) => {
-	console.log(obj.name);
+	scene.add(generateObject(obj));
 });
 
 // const drag = new DragControls(objects, camera, renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
+
+const spotLight = new THREE.SpotLight(0xf5c77f);
+spotLight.castShadow = true;
+scene.add(spotLight);
+
+getTimeScenery(new Date().getHours());
 
 const animate = () => {
 	  if (resizeRendererToDisplaySize(renderer)) {
@@ -59,68 +80,41 @@ const animate = () => {
 	    camera.aspect = canvas.clientWidth / canvas.clientHeight;
 	    camera.updateProjectionMatrix();
 	  }
+
 	// human control: WASD
 	document.addEventListener("keydown", (event) => {
 		if (event.key === "w" || event.key === "ArrowUp") {
-			human.position.y += 0.001;
+			human.position.y += 0.01;
 		} else if (event.key === "a" || event.key === "ArrowLeft") {
-			human.position.x -= 0.001;
+			human.position.x -= 0.01;
 		} else if (event.key === "s" || event.key === "ArrowDown") {
-			human.position.y -= 0.001;
+			human.position.y -= 0.01;
 		} else if (event.key === "d" || event.key === "ArrowRight") {
-			human.position.x += 0.001;
-		}
-
-		const hour = new Date().getHours();
-
-		if (hour >= 6 && hour < 12) {
-	    	scene.background = new THREE.Color(0x2e4482);
-		} else if (hour >= 12 && hour < 19) {
-	    	scene.background = new THREE.Color(0x546bab);
-		} else {
-	    	scene.background = new THREE.Color(0x2e4482);
-	  }
+			human.position.x += 0.01;
+		}		
 	});
+
+	getTimeScenery(new Date().getHours());
 
 	controls.update();
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 };
 
-const addObjectForm = document.querySelector("form");
+const addObjectFromForm = document.querySelector("form");
 
-addObjectForm.addEventListener("submit", (event) => {
+addObjectFromForm.addEventListener("submit", (event) => {
 	event.preventDefault();
 
-	const [name, size, x, y] = [
-		event.target.object.value,
-		event.target.size.value,
-		event.target.x.value,
-		event.target.y.value
-	];
+	const object = {
+		name: event.target.object.value,
+		size: event.target.size.value,
+		x: event.target.x.value,
+		y: event.target.y.value
+	};
 
-	switch (name) {
-		case "tree":
-			const tree = createObject.createTree(x, y);
-			scene.add(tree);
-			objects.push(tree);
-			break;
-
-		case "mountain":
-			const mountain1 = createObject.createMountain(x, y);
-			scene.add(mountain1);
-			objects.push(mountain1);
-			break;
-
-		case "human":
-			const human = createObject.createHumanObject();
-			scene.add(human);
-			objects.push(human);
-			break;
-
-		default:
-			return;
-	}
+	generateObject(object);
+	objects.push(object);
 
 	localStorage.setItem(OBJECT_LOCAL_KEY, JSON.stringify(objects));
 
@@ -130,3 +124,34 @@ addObjectForm.addEventListener("submit", (event) => {
 
 animate();
 
+function generateObject({name, size, x, y}) {
+	switch (name) {
+		case "tree":
+			const tree = createObject.createTree(x, y);
+			scene.add(tree);
+			break;
+
+		case "mountain":
+			const mountain1 = createObject.createMountain(x, y);
+			scene.add(mountain1);
+			break;
+
+		case "mountain2":
+			const mountain2 = createObject.createMountain2(x, y);
+			scene.add(mountain2);
+			break;
+
+		case "mountain3":
+			const mountain3 = createObject.createMountain3(x, y);
+			scene.add(mountain3);
+			break;
+
+		case "human":
+			const human = createObject.createHumanObject();
+			scene.add(human);
+			break;
+
+		default:
+			return;
+	}
+}
